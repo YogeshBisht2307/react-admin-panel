d69529230aad23723b23a03de344f664c9e9df1e
Yogesh Bisht <yogeshbisht.2307@gmail.com>
2021-12-30 01:11:54 +0530

    add project implimented and tested

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import RequireAuth from './components/utility/RequireAuth';
 import AdminLogin from "./pages/admin/admin_login/Login";
 import AdminDashboard from "./pages/admin/admin_dashboard/Dashboard";
 import AdminServices from "./pages/admin/admin_services/Services";
+import AdminProjects from "./pages/admin/admin_projects/Projects";
 import NoRoutePage from "./components/utility/404";
 
 const Home = () => <h1>Home (Public)</h1>;
@@ -21,6 +22,9 @@ const App = () => {
         {/* <Route path="/blog" element={<Blog/>}/> */}
 
         {/* admin panel */}
+        <Route path="admin/login" element={<AdminLogin />} />
+        <Route path="admin/" element={<AdminLogin />} />
+
         <Route
           path="admin/dashboard"
           element={
@@ -40,9 +44,17 @@ const App = () => {
               </DashboardLayout>
             </RequireAuth>
           }
-          />
-        <Route path="admin/login" element={<AdminLogin />} />
-        <Route path="admin/" element={<AdminLogin />} />
+        />
+        <Route
+          path="admin/projects"
+          element={
+            <RequireAuth>
+              <DashboardLayout>
+                <AdminProjects />
+              </DashboardLayout>
+            </RequireAuth>
+          }
+        />
 
         {/* Not found router */}
         <Route
