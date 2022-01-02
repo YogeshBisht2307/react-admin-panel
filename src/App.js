45db2056ac0d4cc1f41083947f3f2629722369bb
Yogesh Bisht <yogeshbisht.2307@gmail.com>
2022-01-02 19:30:54 +0530

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ const App = () => {
                 <AdminProjects />
               </DashboardLayout>
             </RequireAuth>
+          }
         />
         <Route
           path="admin/techstack"
